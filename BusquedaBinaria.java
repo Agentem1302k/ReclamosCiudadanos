7f645c48d1a5d44e6bd4746d4367549a834f124a
Agentem1302k <91393651+Agentem1302k@users.noreply.github.com>
2026-06-27 00:51:25 -0400

    Implement binary search method for Reclamo

--- BusquedaBinaria.java
+++ BusquedaBinaria.java
@@ -1,1 +1,17 @@
+public Reclamo busquedaBinaria(int codigo) {
+        int inicio = 0;
+        int fin = listaReclamos.size() - 1;
 
+        while (inicio <= fin) {
+            int medio = inicio + (fin - inicio) / 2;
+            if (listaReclamos.get(medio).codigo == codigo) {
+                return listaReclamos.get(medio);
+            }
+            if (listaReclamos.get(medio).codigo < codigo) {
+                inicio = medio + 1;
+            } else {
+                fin = medio - 1;
+            }
+        }
+        return null;
+    }
